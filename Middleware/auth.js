25bb39042a8398d59e1fd5aa3cc34a8f718cfce0
Harish6446 <167738136+Harish6446@users.noreply.github.com>
2026-01-25 23:00:50 +0530

    Update auth.js
    Modified logic to implement ACL in middleware itself, instead of hard-coding in the route code

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const {accessControlMatrix} = require("../config/RBACMatrix");
 
 module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
@@ -12,6 +13,14 @@ module.exports = (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
+
+        const allowedRules = accessControlMatrix[req.path];
+
+        if (allowedRules && !allowedRules.includes(req.user.role)){
+            return res.status(403).json({
+                message: `Access denied: ${req.user.role}s cannot access this resource.`
+            });
+        }
         next();
     } catch (err) {
         return res.status(401).json({ message: "Invalid token" });
